c8aff41b3e2828af61d842c3b2367c45afc13634
Richard Kallos <rkallos@gmail.com>
2023-04-19 14:16:32 -0400

    Add StatsD reporting, Options

---
+++ lib/peep/options.ex
@@ -0,0 +1,129 @@
+defmodule Peep.Options do
+  @statsd_opts_schema [
+    flush_interval_ms: [
+      type: :non_neg_integer,
+      default: 5000,
+      doc: "Time interval between StatsD metric emissions."
+    ],
+    host: [
+      type: {:custom, __MODULE__, :host, []},
+      default: {127, 0, 0, 1},
+      doc: "Hostname or IP address of the StatsD server."
+    ],
+    port: [
+      type: :non_neg_integer,
+      default: 8125,
+      doc: "Port of the StatsD server."
+    ],
+    socket_path: [
+      type: {:custom, __MODULE__, :socket_path, []},
+      doc: "Path to the Unix Domain Socket used for publishing instead of the hostname and port."
+    ],
+    formatter: [
+      type: {:custom, __MODULE__, :formatter, []},
+      default: :standard,
+      doc:
+        "Determines the format of the published metrics. Can be either `:standard` or `:datadog`."
+    ],
+    mtu: [
+      type: :non_neg_integer,
+      default: 1472,
+      doc:
+        "Determine max size of statsd packets. For UDP, 1472 is a good choice. For UDS, 8192 is probably better."
+    ]
+  ]
+
+  @schema [
+    metrics: [
+      type: {:list, :any},
+      required: true,
+      doc: "A list of `Telemetry.Metrics` metric definitions to be collected and exposed."
+    ],
+    statsd: [
+      type: {:or, [nil, {:keyword_list, @statsd_opts_schema}]}
+    ],
+    global_tags: [
+      type: :keyword_list,
+      default: [],
+      doc:
+        "Additional tags published with every metric. " <>
+          "Global tags are overriden by the tags specified in the metric definition."
+    ]
+  ]
+
+  defstruct Keyword.keys(@schema)
+
+  @spec docs() :: String.t()
+  def docs do
+    NimbleOptions.docs(@schema)
+  end
+
+  @spec validate(Keyword.t()) :: {:ok, %__MODULE__{}} | {:error, String.t()}
+  def validate(options) do
+    case NimbleOptions.validate(options, @schema) do
+      {:ok, options} ->
+        new_statsd_options = rename_socket_path(options[:statsd])
+        new_options = Keyword.put(options, :statsd, new_statsd_options)
+        {:ok, struct(__MODULE__, new_options)}
+
+      {:error, err} ->
+        {:error, Exception.message(err)}
+    end
+  end
+
+  @spec host(term()) ::
+          {:ok, :inet.ip_address() | :inet.hostname()} | {:error, String.t()}
+  def host(address) when is_tuple(address) do
+    case :inet.ntoa(address) do
+      {:error, _} ->
+        {:error, "expected :host to be a valid IP address, got #{inspect(address)}"}
+
+      _ ->
+        {:ok, address}
+    end
+  end
+
+  def host(address) when is_binary(address) do
+    {:ok, to_charlist(address)}
+  end
+
+  def host(term) do
+    {:error, "expected :host to be an IP address or a hostname, got #{inspect(term)}"}
+  end
+
+  @spec socket_path(term()) :: {:ok, :inet.local_address()} | {:error, String.t()}
+  def socket_path(path) when is_binary(path) do
+    {:ok, {:local, to_charlist(path)}}
+  end
+
+  def socket_path(term) do
+    {:error, "expected :socket_path to be a string, got #{inspect(term)}"}
+  end
+
+  @spec formatter(term()) :: {:ok, :standard | :datadog} | {:error, String.t()}
+  def formatter(:standard) do
+    {:ok, :standard}
+  end
+
+  def formatter(:datadog) do
+    {:ok, :datadog}
+  end
+
+  def formatter(term) do
+    {:error, "expected :formatter be either :standard or :datadog, got #{inspect(term)}"}
+  end
+
+  defp rename_socket_path(nil) do
+    nil
+  end
+
+  defp rename_socket_path(statsd_opts) do
+    if socket_path = Keyword.get(statsd_opts, :socket_path) do
+      statsd_opts
+      |> Keyword.put(:host, socket_path)
+      |> Keyword.delete(:socket_path)
+    else
+      statsd_opts
+    end
+  end
+end
---
+++ lib/peep/statsd.ex
@@ -0,0 +1,299 @@
+defmodule Peep.Statsd do
+  require Logger
+  alias Telemetry.Metrics.{Counter, Distribution, LastValue, Sum}
+
+  defmodule Packet do
+    defstruct metrics: [], lines: [], remaining: nil
+
+    def new(mtu) do
+      %Packet{remaining: mtu}
+    end
+
+    def can_add_metric_lines?(%Packet{remaining: remaining}, new_lines) do
+      remaining - calculate_byte_size(new_lines) >= 0
+    end
+
+    def add_metric_lines(
+          %Packet{metrics: metrics, lines: lines, remaining: remaining} = packet,
+          new_metric,
+          new_lines
+        ) do
+      %Packet{
+        packet
+        | metrics: [new_metric | metrics],
+          lines: [Enum.intersperse(new_lines, ?\n), ?\n | lines],
+          remaining: remaining - calculate_byte_size(new_lines)
+      }
+    end
+
+    def send(%Packet{lines: data}, socket, statsd_opts) do
+      host = statsd_opts[:host]
+
+      port =
+        if match?({:local, _}, host) do
+          0
+        else
+          statsd_opts[:port]
+        end
+
+      case :gen_udp.send(socket, host, port, data) do
+        :ok ->
+          :ok
+
+        {:error, reason} = err ->
+          Logger.error("unable to emit StatsD metrics. reason: #{inspect(reason)}")
+          err
+      end
+    end
+
+    defp calculate_byte_size(lines) do
+      bytes = IO.iodata_length(lines)
+      newlines = length(lines) + 1
+      bytes + newlines
+    end
+  end
+
+  defstruct prev: %{}, statsd_opts: %{}, socket: nil
+
+  def make_state(statsd_opts) do
+    %__MODULE__{statsd_opts: statsd_opts}
+    |> try_to_open_socket()
+  end
+
+  def make_and_send_packets(metrics, %__MODULE__{prev: prev, statsd_opts: statsd_opts} = state) do
+    metrics
+    |> calculate_deltas(prev)
+    |> make_lines(statsd_opts[:formatter])
+    |> make_packets(statsd_opts[:mtu])
+    |> send_packets(metrics, try_to_open_socket(state))
+  end
+
+  def make_packets(metrics, mtu) do
+    make_packets(metrics, mtu, [Packet.new(mtu)])
+  end
+
+  defp make_packets([], _mtu, packets) do
+    Enum.reverse(packets)
+  end
+
+  defp make_packets(
+         [{metric, lines} | rest],
+         mtu,
+         [packet | packets]
+       ) do
+    new_acc =
+      if Packet.can_add_metric_lines?(packet, lines) do
+        new_packet = Packet.add_metric_lines(packet, metric, lines)
+        [new_packet | packets]
+      else
+        new_packet = Packet.new(mtu) |> Packet.add_metric_lines(metric, lines)
+        [new_packet, packet | packets]
+      end
+
+    make_packets(rest, mtu, new_acc)
+  end
+
+  defp send_packets([], _metrics, state) do
+    state
+  end
+
+  defp send_packets(_packets, _metrics, %__MODULE__{socket: nil} = state) do
+    state
+  end
+
+  defp send_packets(
+         [packet | rest],
+         metrics,
+         %__MODULE__{prev: prev, statsd_opts: opts, socket: socket} = state
+       ) do
+    new_state =
+      case Packet.send(packet, socket, opts) do
+        :ok ->
+          successfully_sent_metric_keys =
+            for {metric, _} <- packet.metrics do
+              metric
+            end
+
+          successfully_sent_metrics = Map.take(metrics, successfully_sent_metric_keys)
+
+          %__MODULE__{state | prev: Map.merge(prev, successfully_sent_metrics)}
+
+        {:error, :eagain} ->
+          state
+
+        _ ->
+          %__MODULE__{state | socket: nil}
+      end
+
+    send_packets(rest, metrics, new_state)
+  end
+
+  defp calculate_deltas(current_metrics, previous_metrics) do
+    for {key, _tagged_series} = metric <- current_metrics, reduce: %{} do
+      acc ->
+        delta = calculate_delta(metric, previous_metrics)
+
+        if delta != %{} do
+          Map.put(acc, key, delta)
+        else
+          acc
+        end
+    end
+  end
+
+  defp calculate_delta({%Counter{} = metric, tagged_values}, previous_metrics) do
+    previous_metric = previous_metrics[metric] || %{}
+
+    for {tags, value} <- tagged_values, reduce: %{} do
+      acc ->
+        previous_value = previous_metric[tags] || 0
+        delta = value - previous_value
+
+        if delta != 0 do
+          Map.put(acc, tags, delta)
+        else
+          acc
+        end
+    end
+  end
+
+  defp calculate_delta({%LastValue{}, tagged_values}, _previous_metrics) do
+    for {tags, value} <- tagged_values, into: %{} do
+      {tags, value}
+    end
+  end
+
+  defp calculate_delta({%Sum{} = metric, tagged_values}, previous_metrics) do
+    previous_metric = previous_metrics[metric] || %{}
+
+    for {tags, value} <- tagged_values, reduce: %{} do
+      acc ->
+        previous_value = previous_metric[tags] || 0
+        delta = value - previous_value
+
+        if delta != 0 do
+          Map.put(acc, tags, delta)
+        else
+          acc
+        end
+    end
+  end
+
+  defp calculate_delta({%Distribution{} = metric, tagged_buckets}, previous_metrics) do
+    previous_metric = previous_metrics[metric] || %{}
+
+    for {tags, buckets} <- tagged_buckets, into: %{} do
+      previous_buckets = previous_metric[tags] || %{}
+
+      bucket_deltas =
+        for {bucket_value, count} <- Map.delete(buckets, :sum), reduce: %{} do
+          bucket_acc ->
+            previous_count = previous_buckets[bucket_value] || 0
+            delta = count - previous_count
+
+            if delta != 0 do
+              Map.put(bucket_acc, bucket_value, delta)
+            else
+              bucket_acc
+            end
+        end
+
+      {tags, bucket_deltas}
+    end
+  end
+
+  def make_lines(metrics, formatter) do
+    for metric <- metrics do
+      {metric, format_metric(metric, formatter)}
+    end
+  end
+
+  def format_metric({%Counter{} = metric, tagged_counts}, _formatter) do
+    name = format_name(metric.name)
+
+    Enum.map(tagged_counts, fn {tags, count} ->
+      [name, ?:, n2b(count), "|c", format_tags(tags)]
+    end)
+  end
+
+  def format_metric({%LastValue{} = metric, tagged_values}, _formatter) do
+    name = format_name(metric.name)
+
+    Enum.map(tagged_values, fn {tags, value} ->
+      [name, ?:, n2b(value), "|g", format_tags(tags)]
+    end)
+  end
+
+  def format_metric({%Sum{} = metric, tagged_sums}, _formatter) do
+    name = format_name(metric.name)
+
+    Enum.map(tagged_sums, fn {tags, sum} ->
+      [name, ?:, n2b(sum), "|c", format_tags(tags)]
+    end)
+  end
+
+  def format_metric({%Distribution{} = metric, tagged_buckets}, formatter) do
+    name = format_name(metric.name)
+
+    type_bytes =
+      case formatter do
+        :datadog -> ?d
+        _ -> "ms"
+      end
+
+    Enum.flat_map(tagged_buckets, fn {tags, buckets} ->
+      tags_bytes = format_tags(tags)
+
+      buckets
+      |> Enum.map(fn {bucket_string, count} ->
+        {bucket(bucket_string), count}
+      end)
+      |> Enum.sort()
+      |> Enum.map(fn {bucket, count} ->
+        [name, ?:, to_string(bucket), ?|, type_bytes, "|@", f2b(1.0 / count), tags_bytes]
+      end)
+    end)
+  end
+
+  defp format_name(segments) do
+    Enum.map_intersperse(segments, ?., &a2b/1)
+  end
+
+  defp format_tags([]), do: ""
+
+  defp format_tags(tags) do
+    ["|#" | Enum.map_intersperse(tags, ?,, fn {k, v} -> [a2b(k), ?:, to_string(v)] end)]
+  end
+
+  defp a2b(a), do: :erlang.atom_to_binary(a, :utf8)
+  defp f2b(f), do: :erlang.float_to_binary(f, [:compact, {:decimals, 6}])
+  defp n2b(n) when is_integer(n), do: :erlang.integer_to_binary(n)
+  defp n2b(n) when is_float(n), do: :erlang.float_to_binary(n, [:compact, {:decimals, 2}])
+
+  defp bucket(s) when is_binary(s), do: String.to_float(s)
+  defp bucket(other), do: to_string(other)
+
+  defp try_to_open_socket(%__MODULE__{statsd_opts: statsd_opts, socket: nil} = state) do
+    default_socket_opts = [active: false]
+
+    opts =
+      case statsd_opts[:host] do
+        {:local, _} ->
+          [:local | default_socket_opts]
+
+        _ ->
+          default_socket_opts
+      end
+
+    case :gen_udp.open(0, opts) do
+      {:ok, sock} ->
+        %__MODULE__{state | socket: sock}
+
+      {:error, reason} ->
+        Logger.error("unable to open StatsD socket. reason: #{inspect(reason)}")
+        state
+    end
+  end
+
+  defp try_to_open_socket(state), do: state
+end
