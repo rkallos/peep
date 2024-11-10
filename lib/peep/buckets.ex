7319c79fa1c605250ce7203d0e15a3055b34d47e
Yordis Prieto <yordis.prieto@gmail.com>
2024-11-10 00:32:09 -0500

    feat: allow to set the default bucket calculator
    closes #14

--- lib/peep/buckets.ex
+++ lib/peep/buckets.ex
@@ -1,19 +1,57 @@
 defmodule Peep.Buckets do
-  @default_module Peep.Buckets.Exponential
-  @reporter_option :peep_bucket_calculator
-
   @moduledoc """
   A behavior for histogram bucketing strategies.
 
-  If no bucketing strategy is provided (i.e. #{@reporter_option} is not set in
-  :reporter_options for a `%Telemetry.Metrics.Distribution{}`, then the default
-  is `#{@default_module}`.
+  You can pass a bucketing strategy to a distribution metric in your metrics:
+
+  ```elixir
+  def metrics do
+    [
+      distribution([:bandit, :request, :stop, :duration],
+        tags: [],
+        unit: {:native, :millisecond},
+        reporter_options: [
+          peep_bucket_calculator: Peep.Buckets.PowersOfTen
+        ]
+      )
+    ]
+  end
+
+  If no bucketing strategy is provided is not set in :reporter_options for a
+  `%Telemetry.Metrics.Distribution{}`, then the default is
+  `Peep.Buckets.Exponential`.
+
+  You can change the default bucket calculator, set `:bucket_calculator` in your
+  config.
+
+  ```elixir
+  config :peep, bucket_calculator: Peep.Buckets.PowersOfTen
+  ```
+
+  ## Custom Buckets
 
   If you want custom bucket boundaries, there is `Peep.Buckets.Custom`, which
   uses pattern matching to assign sample measurements to buckets.
+
+  Example:
+
+  ```elixir
+  defmodule MyApp.MyBucket do
+    use Peep.Buckets.Custom,
+      buckets: [10, 100, 1_000]
+  end
+  ```
   """
 
   alias Telemetry.Metrics
+
+  @default_module Application.compile_env(
+                    :peep,
+                    :bucket_calculator,
+                    Peep.Buckets.Exponential
+                  )
+
+  @reporter_option :peep_bucket_calculator
 
   @type config :: map
 
