4d26cf2b68e984efc3a0a31f068b1d4335a1bb98
Richard Kallos <richard@marketops.com>
2025-01-03 11:04:57 -0500

    Bump minimum Elixir version, upgrade Erlang + Elixir in GHA

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Peep.MixProject do
     [
       app: :peep,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
