47d6c7fc10b8b7542371d731a854469a329e564b
Louis-Philippe Gauthier <lpgauth@gmail.com>
2026-03-17 14:33:37 -0400

    Deduplicate tag_fn calls per event via indexed tag results tuple

--- lib/peep/event_handler.ex
+++ lib/peep/event_handler.ex
@@ -42,23 +42,42 @@ defmodule Peep.EventHandler do
   end
 
   def precompute_metrics(metrics, {storage_mod, _}) do
-    Enum.map(metrics, fn {metric, id} ->
-      %{
-        measurement: measurement,
-        tag_values: tag_values,
-        tags: tags,
-        keep: keep
-      } = metric
+    {tag_map, _} =
+      Enum.reduce(metrics, {%{}, 0}, fn {metric, _id}, {map, next_idx} ->
+        key = {metric.tag_values, metric.tags}
 
-      tag_fn = compile_tag_fn(tag_values, tags)
-      keep_val = if is_nil(keep), do: :no_keep, else: keep
+        case map do
+          %{^key => _} -> {map, next_idx}
+          _ -> {Map.put(map, key, next_idx), next_idx + 1}
+        end
+      end)
 
-      insert_fn = fn data, value, tags ->
-        storage_mod.insert_metric(data, id, metric, value, tags)
-      end
+    tag_fns =
+      tag_map
+      |> Enum.sort_by(fn {_key, idx} -> idx end)
+      |> Enum.map(fn {{tag_values, tags}, _idx} -> compile_tag_fn(tag_values, tags) end)
+      |> List.to_tuple()
 
-      {metric_type(metric), insert_fn, keep_val, measurement, tag_fn}
-    end)
+    metrics_list =
+      Enum.map(metrics, fn {metric, id} ->
+        %{
+          measurement: measurement,
+          tag_values: tag_values,
+          tags: tags,
+          keep: keep
+        } = metric
+
+        tag_idx = Map.fetch!(tag_map, {tag_values, tags})
+        keep_val = if is_nil(keep), do: :no_keep, else: keep
+
+        insert_fn = fn data, value, tags ->
+          storage_mod.insert_metric(data, id, metric, value, tags)
+        end
+
+        {metric_type(metric), insert_fn, keep_val, measurement, tag_idx}
+      end)
+
+    {tag_fns, metrics_list}
   end
 
   defp metric_type(%Telemetry.Metrics.Counter{}), do: :counter
@@ -69,71 +88,84 @@ defmodule Peep.EventHandler do
   defp compile_tag_fn(tag_values, keys), do: fn metadata -> Map.take(tag_values.(metadata), keys) end
 
   def handle_event(_event, measurements, metadata, event_key) do
-    {{storage_mod, storage}, metrics} = :persistent_term.get(event_key)
+    {{storage_mod, storage}, {tag_fns, metrics}} = :persistent_term.get(event_key)
     resolved = storage_mod.resolve(storage)
-    store_metrics(metrics, measurements, metadata, resolved)
+    tag_results = compute_tags(tag_fns, metadata, tuple_size(tag_fns), 0, [])
+    store_metrics(metrics, measurements, metadata, resolved, tag_results)
   end
 
-  defp store_metrics([], _measurements, _metadata, _data), do: :ok
+  defp compute_tags(_tag_fns, _metadata, size, size, acc) do
+    acc |> :lists.reverse() |> List.to_tuple()
+  end
+
+  defp compute_tags(tag_fns, metadata, size, idx, acc) do
+    compute_tags(tag_fns, metadata, size, idx + 1, [elem(tag_fns, idx).(metadata) | acc])
+  end
+
+  defp store_metrics([], _measurements, _metadata, _data, _tag_results), do: :ok
 
   defp store_metrics(
-         [{:counter, insert_fn, :no_keep, _measurement, tag_fn} | rest],
+         [{:counter, insert_fn, :no_keep, _measurement, tag_idx} | rest],
          measurements,
          metadata,
-         data
+         data,
+         tag_results
        ) do
-    insert_fn.(data, 1, tag_fn.(metadata))
-    store_metrics(rest, measurements, metadata, data)
+    insert_fn.(data, 1, elem(tag_results, tag_idx))
+    store_metrics(rest, measurements, metadata, data, tag_results)
   end
 
   defp store_metrics(
-         [{_type, insert_fn, :no_keep, measurement, tag_fn} | rest],
+         [{_type, insert_fn, :no_keep, measurement, tag_idx} | rest],
          measurements,
          metadata,
-         data
+         data,
+         tag_results
        ) do
     case fetch_measurement(measurement, measurements, metadata) do
       value when is_number(value) ->
-        insert_fn.(data, value, tag_fn.(metadata))
+        insert_fn.(data, value, elem(tag_results, tag_idx))
 
       _ ->
         nil
     end
 
-    store_metrics(rest, measurements, metadata, data)
+    store_metrics(rest, measurements, metadata, data, tag_results)
   end
 
   defp store_metrics(
-         [{:counter, insert_fn, keep, _measurement, tag_fn} | rest],
+         [{:counter, insert_fn, keep, _measurement, tag_idx} | rest],
          measurements,
          metadata,
-         data
+         data,
+         tag_results
        ) do
     if keep?(keep, metadata, nil) do
-      insert_fn.(data, 1, tag_fn.(metadata))
+      insert_fn.(data, 1, elem(tag_results, tag_idx))
     end
 
-    store_metrics(rest, measurements, metadata, data)
+    store_metrics(rest, measurements, metadata, data, tag_results)
   end
 
   defp store_metrics(
-         [{_type, insert_fn, keep, measurement, tag_fn} | rest],
+         [{_type, insert_fn, keep, measurement, tag_idx} | rest],
          measurements,
          metadata,
-         data
+         data,
+         tag_results
        ) do
     if keep?(keep, metadata, measurement) do
       # credo:disable-for-next-line Credo.Check.Refactor.Nesting
       case fetch_measurement(measurement, measurements, metadata) do
         value when is_number(value) ->
-          insert_fn.(data, value, tag_fn.(metadata))
+          insert_fn.(data, value, elem(tag_results, tag_idx))
 
         _ ->
           nil
       end
     end
 
-    store_metrics(rest, measurements, metadata, data)
+    store_metrics(rest, measurements, metadata, data, tag_results)
   end
 
   defp keep?(keep, metadata, measurement) when is_function(keep, 2),
