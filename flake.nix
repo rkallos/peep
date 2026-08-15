d955593e54e16d6c99df1bb1ec8d88ee119b14aa
Richard Kallos <rkallos@gmail.com>
2026-08-15 14:45:44 -0400

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "peep development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        beamPackages = pkgs.beam29Packages;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            beamPackages.erlang
+            beamPackages.elixir_1_20
+          ];
+        };
+      }
+    );
+}
